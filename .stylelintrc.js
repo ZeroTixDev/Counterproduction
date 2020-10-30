73d454a6d8fdf3446cb118ac6594bd9d3898d88b
iMplode nZ <rg@youxplode.com>
2020-10-29 22:19:19 -0700

    Fixed.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,5 +5,6 @@ module.exports = {
         'order/properties-alphabetical-order': null,
         'rule-empty-line-before': null,
         'max-nesting-depth': 2,
+        'selector-max-compound-selectors': null,
     },
 };
