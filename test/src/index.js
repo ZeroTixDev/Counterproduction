79d8794726f4084edf097a0424b8ecbfe73a5156
iMplode nZ <rg@youxplode.com>
2020-10-29 20:08:27 -0700

    Loading spinner.

--- test/src/index.js
+++ test/src/index.js
@@ -5,8 +5,6 @@ const { mod, m } = require('autofactory-ui/settings.js');
 const root = mod('root', (css, use, $) => {
     css(require('./styles.scss'));
     use(ui.window);
-    use(ui.bracketed);
-    use(ui.button);
     let toView = false;
     setTimeout(() => {
         toView = true;
@@ -16,24 +14,14 @@ const root = mod('root', (css, use, $) => {
         view() {
             if (toView) {
                 return $.window(
-                    $._.windowInterior(
-                        $._.wrapper(
-                            $.bracketed({ hover: true }, $._.interior('Bracketed Text (Opens on hover)')),
-                            $.bracketed(
-                                {
-                                    control: (set) => {
-                                        let enter = false;
-                                        setInterval(() => {
-                                            enter = !enter;
-                                            set(enter);
-                                        }, 1000);
-                                    },
-                                },
-                                $._.interior('Bracketed Text (Changes every second)')
-                            ),
-                            $.button('CLICK ME!'),
-                            $.button({ margin: 30 }, $._.bigButtonInterior('This is a big button.'))
-                        )
+                    $._.scene(
+                        {
+                            style: {
+                                __cubeSize: '100px',
+                                __centerDist: '120px',
+                            },
+                        },
+                        $._.cube($._.front(), $._.right(), $._.back(), $._.left(), $._.top(), $._.bottom())
                     )
                 );
             } else return $._();
