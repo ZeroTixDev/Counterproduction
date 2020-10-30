a30ad686e707bb36b0caa2af61a4c4931ce8937a
iMplode nZ <rg@youxplode.com>
2020-10-29 19:08:17 -0700

    Shuffled licenses and did some really stupid fixes needed for firefox.

--- ui/index.js
+++ ui/index.js
@@ -1,7 +1,13 @@
 'use strict';
 
 require('./global-styles.scss');
+
 const m = require('mithril');
 window.addEventListener('resize', m.redraw);
+
+if (navigator.userAgent.includes('Firefox')) {
+    document.body.classList.add('firefox');
+}
+
 const ctx = require.context('./components', false, /\.js$/);
 ctx.keys().forEach((a) => (exports[/\.\/([^.]*)\.js$/.exec(a)[1]] = ctx(a)));
