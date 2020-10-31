358a390a80c3f8b9f7c2c755959260d4e41119ad
iMplode nZ <rg@youxplode.com>
2020-10-30 20:09:40 -0700

    Start of work on prototype.

---
+++ prototype2/src/_main.rs
@@ -0,0 +1,3 @@
+pub fn main() {
+    autofactory_prototype::main::main();
+}
---
+++ prototype2/src/lib.rs
@@ -0,0 +1,6 @@
+#![allow(incomplete_features)]
+#![feature(arbitrary_self_types)]
+#![feature(generic_associated_types)]
+
+// What's a test? Never heard of such a thing.
+// 0|| pub mod tests;
