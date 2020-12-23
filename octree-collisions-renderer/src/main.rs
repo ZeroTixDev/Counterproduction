8bf91cf3ee3024fbbed9da1694e167643d2f35e1
iMplode nZ <rg@youxplode.com>
2020-12-23 10:58:27 -0800

    Made the thing move.

--- octree-collisions-renderer/src/main.rs
+++ octree-collisions-renderer/src/main.rs
@@ -1,7 +1,19 @@
+#![allow(clippy::type_complexity)]
 use bevy::prelude::*;
+use bevy::render::mesh::Indices;
+use bevy::render::mesh::VertexAttributeValues;
+use bevy::render::pipeline::PrimitiveTopology;
+use bevy::tasks::ComputeTaskPool;
+use bevy::tasks::TaskPool;
 use bevy_orbit_controls::*;
+use building_blocks::mesh::*;
+use building_blocks::prelude::*;
+use counterproduction_core::geometry::FVec;
+use counterproduction_core::geometry::IVec;
+use counterproduction_core::geometry::Rot;
+use counterproduction_core::storage::chunk_map::ChunkStorage;
 use counterproduction_core::storage::*;
-use voxel::SimpleVoxel;
+use voxel::*;
 
 mod voxel;
 
@@ -11,38 +23,211 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(OrbitCameraPlugin)
         .add_startup_system(startup.system())
+        .add_startup_system(startup_create_storage.system())
+        .add_system(move_system.system())
+        .add_system(transform_converter_system.system())
+        .add_system(auto_mesh_system.system())
         .run();
 }
 
-#[allow(dead_code)]
-#[allow(unused_variables)]
-fn generate_cube<S: VoxelStorage>(empty: S) -> S
-where
-    S::T: SimpleVoxel, {
-    todo!()
-}
-
-fn startup(
-    commands: &mut Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
+fn startup(commands: &mut Commands) {
     commands
-        .spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
-            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-            ..Default::default()
-        })
         // light
         .spawn(LightBundle {
-            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0) * 10.0),
             ..Default::default()
         })
         // camera
         .spawn(Camera3dBundle {
-            transform: Transform::from_translation(Vec3::new(-3.0, 3.0, 5.0))
+            transform: Transform::from_translation(Vec3::new(1.0, 1.0, 1.0))
                 .looking_at(Vec3::default(), Vec3::unit_y()),
             ..Default::default()
         })
-        .with(OrbitCamera::default());
+        .with(OrbitCamera {
+            x: 0.0,
+            y: 0.0,
+            distance: 40.0,
+            center: Vec3::zero(),
+            rotate_sensitivity: 1.0,
+            zoom_sensitivity: 0.8,
+        });
+}
+
+fn startup_create_storage(
+    commands: &mut Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let mut storage = ChunkStorage::new(Empty.into(), 16);
+    cube(&mut storage, IVec::new(0, 0, 0), 5);
+    cube(&mut storage, IVec::new(10, 3, 0), 2);
+    cube_rand(&mut storage, IVec::new(-30, 20, -50), 10, 0.2);
+    commands.spawn((
+        Remesh,
+        VoxelMaterial(materials.add(StandardMaterial {
+            albedo: Color::rgb_u8(54, 75, 110),
+            ..Default::default()
+        })),
+        storage,
+        ChunkMeshes(vec![]),
+        FVec::new(10.0, 0.0, 15.0),
+        Rot::identity(),
+        GlobalTransform::default(),
+    ));
+}
+
+struct Remesh;
+struct ChunkMeshes(Vec<Entity>);
+struct VoxelMaterial(Handle<StandardMaterial>);
+
+fn auto_mesh_system(
+    commands: &mut Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    pool: Res<ComputeTaskPool>,
+    mut query: Query<(
+        Entity,
+        &Remesh,
+        &VoxelMaterial,
+        &ChunkStorage<SimpleVoxel>,
+        &mut ChunkMeshes,
+    )>,
+) {
+    for (e, Remesh, VoxelMaterial(material), storage, mut prev_meshes) in query.iter_mut() {
+        for a in prev_meshes.0.iter() {
+            commands.despawn(*a);
+        }
+        let chunk_meshes = generate_meshes(&storage.map, &pool.0);
+        let chunk_meshes = chunk_meshes
+            .into_iter()
+            .map(|m| create_mesh_entity(e, m, commands, material.clone(), &mut meshes))
+            .collect::<Vec<_>>();
+        *prev_meshes = ChunkMeshes(chunk_meshes);
+        commands.remove_one::<Remesh>(e);
+    }
+}
+
+fn move_system(time: Res<Time>, mut query: Query<&mut FVec>) {
+    for mut v in query.iter_mut() {
+        // println!("Moving");
+        *v += FVec::new(1.0, 1.0, 1.0) * time.delta_seconds();
+    }
+}
+
+fn transform_converter_system(
+    commands: &mut Commands,
+    query: Query<(Entity, &FVec, &Rot), Or<(Changed<FVec>, Changed<Rot>)>>,
+) {
+    for (e, v, _r) in query.iter() {
+        println!("Changing");
+        println!("Value: {:?}", v);
+        commands.insert_one(
+            e,
+            Transform {
+                translation: (*v.as_array()).into(),
+                rotation: Quat::identity(),
+                scale: Vec3::one(),
+            },
+        );
+    }
+}
+
+fn cube(
+    storage: &mut impl VoxelStorage<T = SimpleVoxel, Position = IVec>,
+    center: IVec,
+    size: i32,
+) {
+    for a in -size..=size {
+        for b in -size..=size {
+            for c in -size..=size {
+                let pos = center + IVec::new(a, b, c);
+                *storage.get_mut(pos).get_mut() = Solid.into();
+            }
+        }
+    }
+}
+
+fn cube_rand(
+    storage: &mut impl VoxelStorage<T = SimpleVoxel, Position = IVec>,
+    center: IVec,
+    size: i32,
+    chance_filled: f32,
+) {
+    for a in -size..=size {
+        for b in -size..=size {
+            for c in -size..=size {
+                let pos = center + IVec::new(a, b, c);
+                if rand::random::<f32>() < chance_filled {
+                    *storage.get_mut(pos).get_mut() = Solid.into();
+                }
+            }
+        }
+    }
+}
+
+fn create_mesh_entity(
+    parent: Entity,
+    mesh: PosNormMesh,
+    commands: &mut Commands,
+    material: Handle<StandardMaterial>,
+    meshes: &mut Assets<Mesh>,
+) -> Entity {
+    assert_eq!(mesh.positions.len(), mesh.normals.len());
+    let num_vertices = mesh.positions.len();
+
+    let mut render_mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    render_mesh.set_attribute(
+        "Vertex_Position",
+        VertexAttributeValues::Float3(mesh.positions),
+    );
+    render_mesh.set_attribute("Vertex_Normal", VertexAttributeValues::Float3(mesh.normals));
+    render_mesh.set_attribute(
+        "Vertex_Uv",
+        VertexAttributeValues::Float2(vec![[0.0; 2]; num_vertices]),
+    );
+    render_mesh.set_indices(Some(Indices::U32(mesh.indices)));
+
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(render_mesh),
+            material,
+            ..Default::default()
+        })
+        .with(Parent(parent))
+        .current_entity()
+        .unwrap()
+}
+fn generate_meshes<M: Sync>(
+    map_ref: &ChunkHashMap3<SimpleVoxel, M>,
+    pool: &TaskPool,
+) -> Vec<PosNormMesh> {
+    let res = pool.scope(|s| {
+        for chunk_key in map_ref.storage().keys() {
+            s.spawn(async move {
+                let padded_chunk_extent = padded_greedy_quads_chunk_extent(
+                    &map_ref.indexer.extent_for_chunk_at_key(*chunk_key),
+                );
+
+                let mut padded_chunk = Array3::fill(padded_chunk_extent, Empty.into());
+                copy_extent(&padded_chunk_extent, map_ref, &mut padded_chunk);
+
+                // TODO bevy: we could avoid re-allocating the buffers on every call if we had
+                // thread-local storage accessible from this task
+                let mut buffer = GreedyQuadsBuffer::new(padded_chunk_extent);
+                greedy_quads(&padded_chunk, &padded_chunk_extent, &mut buffer);
+
+                let mut mesh = PosNormMesh::default();
+                for group in buffer.quad_groups.iter() {
+                    for (quad, _material) in group.quads.iter() {
+                        group.face.add_quad_to_pos_norm_mesh(&quad, &mut mesh);
+                    }
+                }
+
+                if mesh.is_empty() {
+                    None
+                } else {
+                    Some(mesh)
+                }
+            })
+        }
+    });
+    res.into_iter().filter_map(|x| x).collect()
 }
--- octree-collisions-renderer/src/voxel.rs
+++ octree-collisions-renderer/src/voxel.rs
@@ -1,20 +1,22 @@
 use bevy::prelude::*;
+use building_blocks::mesh::MaterialVoxel;
+use building_blocks::prelude::IsEmpty;
 use enum_dispatch::*;
 
 #[enum_dispatch]
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
-pub enum SimpleVoxelType {
+pub enum SimpleVoxel {
     Empty,
     Solid,
 }
-#[enum_dispatch(SimpleVoxelType)]
-pub trait SimpleVoxel: Copy + Eq {
+#[enum_dispatch(SimpleVoxel)]
+pub trait SimpleVoxelType: Copy + Eq {
     fn color(self) -> Color;
     fn collidable(self) -> bool;
 }
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 pub struct Empty;
-impl SimpleVoxel for Empty {
+impl SimpleVoxelType for Empty {
     fn color(self) -> Color {
         Color::rgba(0.0, 0.0, 0.0, 0.0)
     }
@@ -24,11 +26,20 @@ impl SimpleVoxel for Empty {
 }
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 pub struct Solid;
-impl SimpleVoxel for Solid {
+impl SimpleVoxelType for Solid {
     fn color(self) -> Color {
         Color::rgb(0.22, 0.27, 0.35)
     }
     fn collidable(self) -> bool {
         true
     }
+}
+impl IsEmpty for SimpleVoxel {
+    fn is_empty(&self) -> bool {
+        *self == SimpleVoxel::Empty(Empty)
+    }
+}
+impl MaterialVoxel for SimpleVoxel {
+    type Material = ();
+    fn material(&self) {}
 }
